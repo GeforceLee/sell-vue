b0beba84abf64f784bbdc3325e31949856749d37
Yunlong Li <GeforceLee@AC8233BC.ipt.aol.com>
2017-05-12 18:50:20 +0800

    食品页面

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -16,12 +16,43 @@
         <div class="price">
           <span class="now">${{food.price}}</span><span v-show="food.oldPrice" class="old">{{food.oldPrice}}</span>
         </div>
+        <div class="cartcontrol-wrapper">
+          <cartcontrol :food="food"></cartcontrol>
+        </div>
+        <div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count===0" transition="fade">加入购物车
+        </div>
       </div>
-      <div class="cartcontrol-wrapper">
-        <cartcontrol :food="food"></cartcontrol>
+      <split v-show="food.info"></split>
+      <div class="info" v-show="food.info">
+        <h1 class="title">商品信息</h1>
+        <p class="text">{{food.info}}</p>
       </div>
-      <div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count===0" transition="fade">加入购物车</div>
+      <split></split>
+      <div class="rating">
+        <h1 class="title">商品评价</h1>
+        <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc"
+                      :ratings="food.ratings"></ratingselect>
+        <div class="rating-wrapper">
+          <ul v-show="food.ratings && food.ratings.length">
+            <li v-show="needShow(rating.rateType,rating.text)" v-for="rating in food.ratings"
+                class="rating-item border-1px">
+              <div class="user">
+                <span class="name">{{rating.username}}</span>
+                <img :src="rating.avatar" class="avatar" width="12px" height="12px">
+              </div>
+              <div class="time">{{rating.rateTime | formatDate}}</div>
+              <p class="text">
+                <span
+                  :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>{{rating.text}}
+              </p>
+            </li>
+          </ul>
+          <div class="no-rating" v-show="!food.ratings || !food.ratings.length">暂无评价</div>
+        </div>
+      </div>
+
     </div>
+
   </div>
 </template>
 
@@ -29,6 +60,12 @@
   import Vue from 'Vue';
   import BScroll from 'better-scroll';
   import cartcontrol from 'components/cartcontrol/cartcontrol';
+  import split from 'components/split/split';
+  import ratingselect from 'components/ratingselect/ratingselect';
+  import {formatDate} from 'common/js/date';
+  //  const POSITIVE = 0;
+  //  const NEGATIVE = 1;
+  const All = 2;
   export default {
     props: {
       food: {
@@ -37,12 +74,21 @@
     },
     data() {
       return {
-        showFlag: false
+        showFlag: false,
+        selectType: All,
+        onlyContent: false,
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
+        }
       };
     },
     methods: {
       show() {
         this.showFlag = true;
+        this.selectType = All;
+        this.onlyContent = true;
         this.$nextTick(() => {
           if (!this.scroll) {
             this.scroll = new BScroll(this.$els.food, {
@@ -62,15 +108,50 @@
         }
         this.$dispatch('cart.add', event.target);
         Vue.set(this.food, 'count', 1);
+      },
+      needShow(type, text) {
+        console.log(text, type);
+        if (this.onlyContent && !text) {
+          return false;
+        }
+        if (this.selectType === All) {
+          return true;
+        } else {
+          return type === this.selectType;
+        }
       }
     },
     components: {
-      cartcontrol
+      cartcontrol,
+      split,
+      ratingselect
+    },
+    events: {
+      'ratingtype.select'(type) {
+        this.selectType = type;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
+      },
+      'content.toggle'(onlyContent) {
+        this.onlyContent = onlyContent;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
+      }
+    },
+    filters: {
+      formatDate(time) {
+        let date = new Date(time);
+        return formatDate(date, 'yyyy-MM-dd hh:mm:ss');
+      }
     }
   };
 </script>
 
 <style lang="scss">
+  @import "../../common/scss/mixin";
+
   .food {
     position: fixed;
     left: 0;
@@ -112,6 +193,7 @@
     }
     .content {
       padding: 18px;
+      position: relative;
       .title {
         line-height: 14px;
         margin-bottom: 8px;
@@ -146,32 +228,110 @@
           color: rgb(147, 153, 159);
         }
       }
-    }
-    .cartcontrol-wrapper {
-      position: absolute;
-      right: 12px;
-      bottom: 12px;
-    }
-    .buy {
-      position: absolute;
-      right: 18px;
-      bottom: 18px;
-      z-index: 10;
-      height: 24px;
-      line-height: 24px;
-      padding: 0 12px;
-      box-sizing: border-box;
-      border-radius: 12px;
-      font-size: 10px;
-      color: #fff;
-      background: rgb(0, 160, 220);
-      &.fade-transition {
-        transition: all 0.2s;
-        opacity: 1;
+      .cartcontrol-wrapper {
+        position: absolute;
+        right: 12px;
+        bottom: 12px;
       }
-      &.fade-enter, &.fade-leave {
-        opacity: 0;
+      .buy {
+        position: absolute;
+        right: 18px;
+        bottom: 18px;
+        z-index: 10;
+        height: 24px;
+        line-height: 24px;
+        padding: 0 12px;
+        box-sizing: border-box;
+        border-radius: 12px;
+        font-size: 10px;
+        color: #fff;
+        background: rgb(0, 160, 220);
+        &.fade-transition {
+          transition: all 0.2s;
+          opacity: 1;
+        }
+        &.fade-enter, &.fade-leave {
+          opacity: 0;
+        }
       }
     }
+    .info {
+      padding: 18px;
+      .title {
+        line-height: 14px;
+        margin-bottom: 16px;
+        font-size: 14px;
+        color: rgb(7, 17, 27);
+      }
+      .text {
+        line-height: 24px;
+        padding: 0 8px;
+        font-size: 12px;
+        color: rgb(77, 85, 93);
+      }
+    }
+    .rating {
+      padding-top: 18px;
+      .title {
+        line-height: 14px;
+        margin-left: 18px;
+        font-size: 14px;
+        color: rgb(7, 17, 27);
+      }
+      .rating-wrapper {
+        padding: 0 18px;
+        .rating-item {
+          position: relative;
+          padding: 16px 0;
+          @include border-1px(rgba(7, 17, 27, 0.1));
+          .user {
+            position: absolute;
+            right: 0;
+            top: 16px;
+            line-height: 12px;
+            font-size: 0;
+            .name {
+              display: inline-block;
+              margin-right: 6px;
+              vertical-align: top;
+              font-size: 10px;
+              color: rgb(147, 153, 159);
+            }
+            .avatar {
+              border-radius: 50%;
+            }
+
+          }
+          .time {
+            margin-bottom: 6px;
+            line-height: 12px;
+            font-size: 10px;
+            color: rgb(147, 153, 159);
+          }
+          .text {
+            line-height: 16px;
+            font-size: 12px;
+            color: rgb(7, 17, 27);
+            .icon-thumb_up, .icon-thumb_down {
+              margin-right: 4px;
+              /*line-height: 24px;*/
+              font-size: 12px;
+            }
+            .icon-thumb_up {
+              color: rgb(0, 160, 220);
+            }
+            .icon-thumb_down {
+              color: rgb(7, 17, 27);
+            }
+          }
+        }
+        .no-rating {
+          padding: 16px 0;
+          font-size: 12px;
+          color: rgb(147, 153, 159);
+        }
+      }
+    }
+
   }
 </style>
